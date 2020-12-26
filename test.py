45a7c194c0237fa25cce15660f0b182c7f3d13de
Xie Chen <xieche@microsoft.com>
2020-12-26 00:45:02 -0800

    initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+import os
+import math
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
