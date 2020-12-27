f0c760b691f39533934000358a062e407cd317f2
Xie Chen <xieche@microsoft.com>
2020-12-26 19:58:48 -0800

    add the interactive python demo into repo

--- test.py
+++ test.py
@@ -6,3 +6,24 @@ import math
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+
+
+# %%
+import requests
+# %%
+print ("xiexie")
+# %%
+fname = "testfile"
+print (f"{fname}")
+# %%
+os.listdir()
+# %%
+import os
+%pylab inline
+# %%
+plot([0, 1, 2], [0, 1, 4])
+# %%
+import pandas as pd 
+df = pd.DataFrame({"one": 1, "two": 2}, {"one": 11, "two": 22})
+
+# %%
